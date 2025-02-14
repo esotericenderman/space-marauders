32a0139cab8e6d17298dd8c6f859ba9675d7be97
Esoteric Enderman <esotericenderman@gmail.com>
2025-02-14 21:25:43 +0000

    Don't rotate image

--- src/main.kt
+++ src/main.kt
@@ -17,19 +17,10 @@ suspend fun main() = Korge(windowSize = Size(512, 512), backgroundColor = Colors
 
 class MyScene : Scene() {
 	override suspend fun SContainer.sceneMain() {
-		val minDegrees = (-16).degrees
-		val maxDegrees = (+16).degrees
-
 		val image = image(resourcesVfs["ship.png"].readBitmap()) {
-			rotation = maxDegrees
 			anchor(.5, .5)
 			scale(0.8)
 			position(256, 256)
-		}
-
-		while (true) {
-			image.tween(image::rotation[minDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
-			image.tween(image::rotation[maxDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
 		}
 	}
 }
